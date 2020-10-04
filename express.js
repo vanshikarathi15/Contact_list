07be3af73965ba9ed92f4425a475b395f4467444
Vanshika Rathi <vaanshikarathi@gmail.com>
2020-10-04 08:44:39 +0530

    learning parsing

--- express.js
+++ express.js
@@ -5,10 +5,31 @@ const app=express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+var contactList=[
+    {
+        name:"Vanshika",
+        phone:"9999999999"
+    },
+    {
+        name:"Bhavya",
+        phone:"9898989898"
+    },
+    {
+        name:"Mentor",
+        phone:"123456789"
+    }
+]
 
 app.get('/',function(req,res){
-    return res.render('home');
+    return res.render('home',{
+        contact_list:contactList
+    });
 });
+
+app.post("/create-contact" ,function(req,res){
+    console.log(req.body)
+})
 
 app.listen(port,function(err){
     if (err){
